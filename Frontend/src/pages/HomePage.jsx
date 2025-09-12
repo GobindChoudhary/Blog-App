2d44fa49eb96d36117e67f6a49708867e6a47467
GobindChoudhary <chaudharygobind24@gmail.com>
2025-09-12 21:52:59 +0530

    latest blog completed

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
 import AnimationWrapper from "../common/AnimationWrapper";
 import InPageNavigation from "../components/InPageNavigation";
-
+import axios from "axios";
+import BlogsCards from "../components/BlogsCards";
+import Loader from "../common/Loader";
 const HomePage = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [trendingBlogs, setTrendingBlogs] = useState(null);
+  //  latest blog
+  const fetchLatestBlog = () => {
+    axios
+      .get(import.meta.env.VITE_SERVER_DOMAIN + "blogs/latest-blogs")
+      .then(({ data }) => {
+        setBlogs(data.blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  
+  //  trending blog
+  const fetchTrendingBlog = () => {
+    axios
+      .get(import.meta.env.VITE_SERVER_DOMAIN + "blogs/trending-blogs")
+      .then(({ data }) => {
+        setTrendingBlogs(data.blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    fetchLatestBlog();
+    fetchTrendingBlog();
+  }, []);
+
   return (
     <AnimationWrapper>
       <section className="h-cover flex justify-center gap-10">
@@ -11,7 +44,25 @@ const HomePage = () => {
             routes={["home", "trending blogs"]}
             defaultHidden={["trending blogs"]}
           >
-            <div className="">Latest blogs</div>
+            <>
+              {blogs == null ? (
+                <Loader />
+              ) : (
+                blogs.map((blog, i) => {
+                  return (
+                    <AnimationWrapper
+                      transition={{ duration: 1, delay: i * 0.1 }}
+                      key={i}
+                    >
+                      <BlogsCards
+                        content={blog}
+                        author={blog.author.personal_info}
+                      />
+                    </AnimationWrapper>
+                  );
+                })
+              )}
+            </>
             <div className="">Trending blogs</div>
           </InPageNavigation>
         </div>
