d04963471c8be0de0eb4587411b56919b7dcd925
GobindChoudhary <chaudharygobind24@gmail.com>
2025-12-23 16:39:16 +0530

    improve urlPattern for api call and image

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -74,9 +73,30 @@ export default defineConfig({
       },
       workbox: {
         runtimeCaching: [
+          // 🔵 API requests (blogs, comments, users)
           {
-            urlPattern: "*",
+            urlPattern: ({ request }) => request.destination === "",
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "api-cache",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60, // 1 hour
+              },
+            },
+          },
+
+          // 🟢 Images & static assets
+          {
+            urlPattern: /\.(png|jpg|jpeg|svg|webp|ico)$/i,
             handler: "CacheFirst",
+            options: {
+              cacheName: "image-cache",
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+              },
+            },
           },
         ],
       },
